57ff53f3ee2b901aa38179d42d1603ff0bb7b662
Nikkune <nikkune.dev@protonmail.com>
2024-11-18 14:10:51 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -42,37 +42,4 @@ public class AnalyticsCounter {
     public void writeSymptoms(Map<String, Integer> symptoms) {
         writer.writeSymptoms(symptoms);
     }
-
-    private static int headacheCount = 0;
-    private static int rashCount = 0;
-    private static int pupilCount = 0;
-
-    public static void main(String[] args) throws Exception {
-        // first read input
-        BufferedReader reader = new BufferedReader(new FileReader("D:/Projects/rangoly-nicolas-debug-Java/Project02Eclipse/symptoms.txt"));
-        String line = reader.readLine();
-
-        while (line != null) {
-            System.out.println("symptom from file: " + line);
-            if (line.equals("headache")) {
-                headacheCount++;
-                System.out.println("number of headaches: " + headacheCount);
-            } else if (line.equals("rash")) {
-                rashCount++;
-            } else if (line.equals("dialated pupils")) {
-                pupilCount++;
-            }
-
-            line = reader.readLine();    // read next line
-        }
-
-        reader.close();
-
-        // next generate output
-        FileWriter writer = new FileWriter("result.out");
-        writer.write("headache: " + headacheCount + "\n");
-        writer.write("rash: " + rashCount + "\n");
-        writer.write("dialated pupils: " + pupilCount + "\n");
-        writer.close();
-    }
 }
